4bb913e0e73209ca3f41aca14496727958b7e692
sellyempe <145539485+sellyempe@users.noreply.github.com>
2023-09-29 18:23:48 +0700

    initial jurnal01 commit

---
+++ Jurnal01.java
@@ -0,0 +1,9 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Selly Melani Putri");
+        System.out.println("607062300024");
+        System.out.println("Kelas: D3IF-47-01");
+        
+    }
+
+}
